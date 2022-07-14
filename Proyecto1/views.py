4dab3816d232af5f6c55f8ed9053b6901ff4c0d9
Mikebainbain <mlopez749@gmail.com>
2022-07-14 16:14:21 -0500

    Variables creadas en views y mandadas a llamar desde un archivo html , tambien creacion del constructor

--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -2,11 +2,25 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.template import Template,Context
 
+# Constructor
+class Persona(object):
+    def __init__(self, nombre, apellido):
+        self.nombre=nombre
+        self.apellido=apellido
+
 def saludo(request):
+
+    p1=Persona("Angel", "Barrera")
+
+    nombre = "Miguel" 
+    apellido = "Lopez"
+    edad = 25
+    val_fun_fecha = datetime.now()  #se puede colocar el valor que le queremos asignar directamente  osea a lado de el valor clave
+    
     doc_externo=open("C:/Users/ur513.dsi/Desktop/Proyectos_django/Proyecto1/Proyecto1/Plantillas/index.html") 
     plt=Template(doc_externo.read())
     doc_externo.close()
-    ctx=Context()
+    ctx=Context({"val_nombre": p1.nombre,"val_apellido":p1.apellido , "val_edad": edad, "val_fecha" :val_fun_fecha })
     documento = plt.render( ctx)
     return HttpResponse(documento)
 
